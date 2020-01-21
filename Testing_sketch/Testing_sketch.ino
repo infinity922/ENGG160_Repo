94ac9d34d4b8fe12ca033e80e36443e28e18c061
Infinity922 <ethanmcghan7@gmail.com>
2020-01-21 09:20:14 -0700

    Test file modified

--- Testing_sketch/Testing_sketch.ino
+++ Testing_sketch/Testing_sketch.ino
@@ -1,9 +1,25 @@
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+Zumo32U4LCD lcd;
+Zumo32U4Motors motors;
+Zumo32U4ButtonA buttonA;
+Zumo32U4LineSensors ls;
+
+
 void setup() {
   // put your setup code here, to run once:
+  lcd.clear();
+  ls.initFiveSensors();
+  lcd.print(F("Press A"));
+  buttonA.waitForButton();
+  lcd.clear();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  motors.setSpeeds(400,400);
+  
 
 }
