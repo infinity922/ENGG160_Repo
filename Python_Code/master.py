2974c13353b3883218d4dbe6b0c0a4d68c700814
Infinity922 <ethanmcghan7@gmail.com>
2020-03-03 10:43:04 -0700

    Drive class added

--- Python_Code/master.py
+++ Python_Code/master.py
@@ -2,9 +2,12 @@ import pyfirmata
 import drive
 import time
 
+
 bot = pyfirmata.Arduino('COM4');
 
 driver = drive.Drive(bot)
-driver.tankDrive(0.5,-0.5)
-time.sleep(5)
-driver.stop()
+# driver.tankDrive(0.5,-0.5)
+# time.sleep(5)
+# driver.stop()
+# compass = bot.digital_ports[2]
+# compass.enable_reporting()
