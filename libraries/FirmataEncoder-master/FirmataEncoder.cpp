23798ea10efc826673d0eb9ef340f4c7802740fa
Infinity922 <ethanmcghan7@gmail.com>
2020-03-07 22:48:42 -0700

    Line Sensors added

--- libraries/FirmataEncoder-master/FirmataEncoder.cpp
+++ libraries/FirmataEncoder-master/FirmataEncoder.cpp
@@ -126,6 +126,7 @@ void FirmataEncoder::reportPosition(byte encoder)
 }
 
 void FirmataEncoder::reportLineSensors() {
+    lineSensors.emittersOn();
     lineSensors.read(lineSensorValues);
     short int sensor0 = lineSensorValues[0];
     short int sensor1 = lineSensorValues[1];
