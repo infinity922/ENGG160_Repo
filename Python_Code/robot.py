e90806a7960a32299668b5719f5c68c147695aad
Infinity922 <ethanmcghan7@gmail.com>
2020-03-09 12:16:52 -0600

    added documentation to the robot.py file

--- Python_Code/robot.py
+++ Python_Code/robot.py
@@ -11,11 +11,14 @@ ZUMO = 0x61
 class Robot:
 
     def __init__(self):
+        """This method is automatically called when robot is created, there shouldn't be any need
+         to run it after that"""
         self.board = my_pyfirmata.Arduino('COM4')
         # replace this address with the one from your Arduino IDE
         # For the pi: /dev/ttyACM0
 
     def set_left_motor(self, power):
+        """This method sets a power to the left motor in range (-1, 1)"""
         if power > 1:
             power = 1
         elif power < -1:
@@ -29,6 +32,7 @@ class Robot:
         self.board.sp.write(bytearray([START_SYSEX, ZUMO, LEFT_MOTOR_POWER, power % 128, (power >> 7), dir, END_SYSEX]))
 
     def set_right_motor(self, power):
+        """This method sets a power to the right motor in range (-1, 1)"""
         if power > 1:
             power = 1
         elif power < -1:
@@ -42,6 +46,7 @@ class Robot:
         self.board.sp.write(bytearray([START_SYSEX, ZUMO, RIGHT_MOTOR_POWER, power % 128, power >> 7, dir, END_SYSEX]))
 
     def get_right_encoder(self):
+        """This class gets the number of ticks from the right encoder since the last encoder reset as an integer"""
         self.board.sp.write(bytearray([START_SYSEX, ZUMO, ENCODER_REPORT_POSTITON, 1, END_SYSEX]))
         recived = False
         while recived is False:
@@ -53,6 +58,7 @@ class Robot:
                 return enc_dat[1]
 
     def get_left_encoder(self):
+        """This method gets the number of ticks from the left encoder since the last encoder reset as an integer"""
         self.board.sp.write(bytearray([START_SYSEX, ZUMO, ENCODER_REPORT_POSTITON, 0, END_SYSEX]))
 
         recived = False
@@ -65,6 +71,7 @@ class Robot:
                 return enc_dat[0]
 
     def get_encoders(self):
+        """this method gets the number of ticks from both encoders in a 1x2 array of integers"""
         self.board.sp.write(bytearray([START_SYSEX, ZUMO, ENCODER_REPORT_POSTITON, 0, END_SYSEX]))
 
         recived = False
@@ -77,9 +84,11 @@ class Robot:
                 return enc_dat
 
     def reset_encoders(self):
+        """This method resets the encoders"""
         self.board.sp.write(bytearray([START_SYSEX,ZUMO,ENCODER_RESET_POSITION, 1, END_SYSEX]))
 
     def get_lines(self):
+        """This method gets the values from the line sensors as a 1x3 array"""
         self.board.sp.write(bytearray([START_SYSEX,ZUMO,LINE_SENSORS,END_SYSEX]))
 
         recived = False
