9c3a53f8ff0ee3816dccfb7b781a11daa07869bc
Infinity922 <ethanmcghan7@gmail.com>
2020-03-31 20:29:15 -0600

    getting master built

---
+++ ethan_testing.py
@@ -0,0 +1,38 @@
+from Python_Code.drive import Drive
+from Python_Code.robot import Robot
+from Python_Code.navigation import Navigation
+import time
+
+r = Robot()  # initialize the robot
+driver = Drive(r)  # initialize the driver
+nav = Navigation(r, driver)  # initialize the nav
+
+startTime = time.time()
+
+timeout = 300
+
+running = True
+state = 0
+
+while running:
+
+    if r.board.bytes_available():
+        while r.board.bytes_available():
+            r.board.iterate()
+    if time.time() - startTime > timeout:
+        print("timed out")
+        running = False
+    driver.iterate()
+    nav.iterate()
+
+    if state == 0:
+        driver.startEncoderDrive(300,300)
+        state = 1
+    elif state == 1:
+        if driver.targetReached:
+            state = 2
+    elif state == 2:
+        running = False
+
+driver.stop()
+print('done')
