28dbfb6338cfbc6c8d06e4abbef136fd40d50da5
Infinity922 <ethanmcghan7@gmail.com>
2020-04-02 10:47:38 -0600

    getting master built

--- Python_Code/navigation.py
+++ Python_Code/navigation.py
@@ -70,7 +70,7 @@ class Navigation:
 
             elif not self.foundBlack:
                 self.driver.tankDrive(.3,.3)
-                if lines[1] < THRESHOLD | lines[0] < THRESHOLD | lines[2] < THRESHOLD:
+                if (lines[1] < THRESHOLD) | (lines[0] < THRESHOLD) | (lines[2] < THRESHOLD):
                     self.foundBlack = True
             if lines[0] < THRESHOLD + TOLERANCE & lines[0] > THRESHOLD - TOLERANCE & lines[1] < THRESHOLD + TOLERANCE & lines[1] > THRESHOLD - TOLERANCE & lines[2] < THRESHOLD + TOLERANCE & lines[2] > THRESHOLD - TOLERANCE:
                 self.finishedSquare = True
---
+++ stop.py
@@ -0,0 +1,6 @@
+from Python_Code import Drive
+from Python_Code import Robot
+
+r = Robot()
+driver = Drive(r)
+driver.stop()
