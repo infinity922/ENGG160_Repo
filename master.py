0a33e1a642c46b03b6f979ca31eeb5230c918482
Infinity922 <ethanmcghan7@gmail.com>
2020-04-02 18:19:19 -0600

    getting master built

--- master.py
+++ master.py
@@ -416,6 +416,8 @@ while running:
     elif state == TEST:
         lin = r.get_lines()
         print(math.floor(lin[0]), math.floor(lin[2]), 'ahhha')
+        if nav.squareUp(IN_FRONT):
+            state = STOP
 
 
 
